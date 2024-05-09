78b8db192a8f3322638790e3ba970aa8af1aa2d1
Gatoxic0 <re.romanini@duocuc.cl>
2024-05-08 23:09:31 -0400

    Modificaciones pagina pedidos personalizados

--- funciones.js
+++ funciones.js
@@ -17,25 +17,27 @@ function ValidarCelular(){
     if(input.value.length >= 9){
         input.classList.add("valido")
         input.classList.remove("no-valido")
-        document.querySelector("#errornombre").innerHTML = "&nbsp";
+        document.querySelector("#errorcelular").innerHTML = "&nbsp";
     }else{
         input.classList.add("no-valido")
         input.classList.remove("valido")
-        document.querySelector("#errornombre").innerHTML = "Error, ingrese un mínimo de 10 carácteres"
+        document.querySelector("#errorcel").innerHTML = "Error, ingrese un mínimo de 9 numeros"
 
     }
 }
 
 function ValidarCorreo(){
     let input=document.querySelector("#correo");
-    if(input.value.length >= 13){
+    if(input.value.length >= 11){
         input.classList.add("valido")
         input.classList.remove("no-valido")
-        document.querySelector("#errornombre").innerHTML = "&nbsp";
+        document.querySelector("#errorcorreo").innerHTML = "&nbsp";
     }else{
         input.classList.add("no-valido")
         input.classList.remove("valido")
-        document.querySelector("#errornombre").innerHTML = "Error, ingrese un mínimo de 10 carácteres"
+        document.querySelector("#errorcorreo").innerHTML = "Error, ingrese un mínimo de 11 carácteres"
 
     }
 }
+
+
