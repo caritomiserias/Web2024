0eb75e2be6e3e1dca75d5d20695ec21505bce21b
caritomiserias <caro.avila@duocuc.cl>
2024-05-14 20:51:18 -0400

    arreglo responsividad

--- apis.js
+++ apis.js
@@ -1,39 +1,26 @@
 function getusuarios(){
-    fetch('https://api.escuelajs.co/api/v1/users?limit=3').then(function(response) {
+    fetch('https://api.escuelajs.co/api/v1/users?limit=3')
+    .then(function(response) {
         return response.json();
-    }).then(function(data) {
-        document.querySelector("#review1").src=data[0].avatar;
-        document.querySelector("#name1").innerHTML=data[0].name;
-
-
-        document.querySelector("#review2").src=data[1].avatar;
-        document.querySelector("#name2").innerHTML=data[1].name;
-
-
-        document.querySelector("#review3").src=data[2].avatar;
-        document.querySelector("#name3").innerHTML=data[1].name;
-
-
+    })
+    .then(function(data) {
+        for(let i = 0; i < 3; i++){
+            document.querySelector("#review"+(i+1)).src=data[i].avatar;
+            document.querySelector("#name"+(i+1)).innerHTML=data[i].name;
+        }
     }).catch(function(error) {
         console.log('Requestfailed', error);
     });
 
-    fetch('https://official-joke-api.appspot.com/jokes/ten').then(function(response) {
+    fetch('https://official-joke-api.appspot.com/jokes/ten')
+    .then(function(response) {
         return response.json();
     }).then(function(data) {
-        document.querySelector("#joke1").innerHTML=data[0].setup;
-        document.querySelector("#punchline1").innerHTML=data[0].punchline;
-
-        document.querySelector("#joke2").innerHTML=data[1].setup;
-        document.querySelector("#punchline2").innerHTML=data[1].punchline;
-
-
-        document.querySelector("#joke3").innerHTML=data[2].setup;
-        document.querySelector("#punchline3").innerHTML=data[2].punchline;
-
+        for(let i = 0; i < 3; i++){
+            document.querySelector("#joke"+(i+1)).innerHTML=data[i].setup;
+            document.querySelector("#punchline"+(i+1)).innerHTML=data[i].punchline;
+        }
         document.querySelector("#reviews").style.display = 'block';
-
-
     }).catch(function(error) {
         console.log('Requestfailed', error);
     });
